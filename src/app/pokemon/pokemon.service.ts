cc3c7a017473d7c95241bc6ceb1e621efb829233
meleadr <meledina.adriano@gmail.com>
2023-02-04 17:22:36 +0100

    Ajout de la possibilité d'ajouter un nouveau pokémon

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -33,6 +33,17 @@ export class PokemonService {
 		);
 	}
 
+	addPokemon(pokemon: Pokemon): Observable<Pokemon>{
+		const httpOptions = {
+			headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+		};
+
+		return this.http.post<Pokemon>(`api/pokemons`, pokemon, httpOptions).pipe(
+			tap((response) => this.log(response)),
+			catchError((error) => this.handleError(error, null))
+		);
+	}
+
 	deletePokemonById(pokemonId: number): Observable<null>{
 		return this.http.delete(`api/pokemons/${pokemonId}`).pipe(
 			tap((response) => this.log(response)),
